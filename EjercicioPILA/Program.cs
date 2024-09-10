babd3bd03fc3bd92bd52b26c015215079811fc43
Bruno Sponton <brunogsponton@hotmail.com>
2024-09-09 21:07:31 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioPILA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class EjercicioPILA
+{
+    static void Main()
+    {
+        Stack<string> pila = new Stack<string>();
+
+        while (true)
+        {
+            Console.Write("Introducir una cadena (presionar Enter para terminar): ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+
+            pila.Push(input);
+        }
+
+        Console.WriteLine("Cadenas introducidas en orden inverso:");
+
+        while (pila.Count > 0)
+        {
+            string cadena = pila.Pop();
+            Console.WriteLine(cadena);
+        }
+    }
+} 
